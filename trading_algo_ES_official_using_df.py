c7574cd75ed889b6de9b79bfe26337f854df1595
koffeewithkha <75348796+koffeewithkha@users.noreply.github.com>
2021-09-16 09:04:23 -0700

    Added ROC, CCI to the existing DF, so we have all indicators in the same DataFrame now.

--- trading_algo_ES_official_using_df.py
+++ trading_algo_ES_official_using_df.py
@@ -11,12 +11,12 @@ from ibapi.wrapper import EWrapper
 
 # Using WMA for Slow & HMA for Fast indicator for both TF1 & TF2 time frames
 
-TICKS_PER_CANDLE_TF1 = 512  #10 #34 #55 #89 #144 #233 #377
-MOVING_AVG_PERIOD_LENGTH_TF1_S = 14 #9 #14 # slow timeframe (WMA)
-MOVING_AVG_PERIOD_LENGTH_TF1_F = 14 #5 #9 # fast timeframe (WMA)
-TICKS_PER_CANDLE_TF2 = 144 #5 #10 #34 #55 #89 #144 #233 #377
-MOVING_AVG_PERIOD_LENGTH_TF2_S = 9 #9 #14 (WMA)
-MOVING_AVG_PERIOD_LENGTH_TF2_F = 9 #5 #9 (WMA)
+TICKS_PER_CANDLE_TF1 = 144  #10 #34 #55 #89 #144 #233 #377
+MOVING_AVG_PERIOD_LENGTH_TF1_S = 9 #9 #14 # slow timeframe (WMA)
+MOVING_AVG_PERIOD_LENGTH_TF1_F = 9 #5 #9 # fast timeframe (WMA)
+TICKS_PER_CANDLE_TF2 = 89 #5 #10 #34 #55 #89 #144 #233 #377
+MOVING_AVG_PERIOD_LENGTH_TF2_S = 5 #9 #14 (WMA)
+MOVING_AVG_PERIOD_LENGTH_TF2_F = 5 #5 #9 (WMA)
 
 class TestApp(EWrapper, EClient):
     def __init__(self):
@@ -148,37 +148,30 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf1_s['low']  = self.df_indicator_tf1_s['close']
         self.df_indicator_tf1_s['indicator_tf1_s'] = TA.WMA(self.df_indicator_tf1_s, self.mov_avg_length_tf1_s) # choose indicator here
         self.indicator_tf1_s = self.df_indicator_tf1_s['indicator_tf1_s'].iloc[-1]
-        print("\nSadiq: Data_TF1_S =\n", self.data_tf1_s, "\n", "df_indicator_tf1_s =\n", self.df_indicator_tf1_s, "\n", "indicator_tf1_s =", self.indicator_tf1_s, "\n")
+        #print("\nSadiq: Data_TF1_S =\n", self.data_tf1_s, "\n", "df_indicator_tf1_s =\n", self.df_indicator_tf1_s, "\n", "indicator_tf1_s =", self.indicator_tf1_s, "\n")
 
         #Populating the DF with ROC
         self.roc_counter_tf1_s += 1
-        print("\nSadiq (ROC Pre) Data_TF1_S =", self.data_tf1_s, "ROC_Counter_TF1_S =", self.roc_counter_tf1_s, "Data_TF1_S_LEN =", len(self.data_tf1_s), "mov_avg_length_tf1_s", self.mov_avg_length_tf1_s)
+        #print("\nSadiq (ROC Pre) Data_TF1_S =", self.data_tf1_s, "ROC_Counter_TF1_S =", self.roc_counter_tf1_s, "Data_TF1_S_LEN =", len(self.data_tf1_s), "mov_avg_length_tf1_s", self.mov_avg_length_tf1_s)
         if self.roc_counter_tf1_s < self.mov_avg_length_tf1_s:
             return
-
-        #print("Debug 0", self.df_indicator_tf1_s)
-        #print("Debug 1", self.df_indicator_tf1_s['close'])
-        #print("Debug 2", self.df_indicator_tf1_s['close'].diff(self.mov_avg_length_tf1_s))
-        #print("Debug 3", self.df_indicator_tf1_s['close'].diff(self.mov_avg_length_tf1_s - 1))
-        #print("Debug 4", self.df_indicator_tf1_s['close'].shift(self.mov_avg_length_tf1_s))
-        #print("Debug 4", self.df_indicator_tf1_s['close'].shift(self.mov_avg_length_tf1_s - 1))
-
         if len(self.data_tf1_s) >= self.mov_avg_length_tf1_s:
             self.df_indicator_tf1_s['ROC_M'] = self.df_indicator_tf1_s['close'].diff(self.mov_avg_length_tf1_s - 1)
             self.df_indicator_tf1_s['ROC_N'] = self.df_indicator_tf1_s['close'].shift(self.mov_avg_length_tf1_s - 1)
             self.df_indicator_tf1_s['ROC'] = (self.df_indicator_tf1_s['ROC_M'] / self.df_indicator_tf1_s['ROC_N']) * 100
-            print("\nSadiq: (ROC Post)) Data_TF1_S =\n", self.data_tf1_s, "\n", "df_indicator_tf1_s =\n", self.df_indicator_tf1_s, "\n", "indicator_tf1_s =", self.indicator_tf1_s, "\n")
+            #print("\nSadiq: (ROC Post)) Data_TF1_S =\n", self.data_tf1_s, "\n", "df_indicator_tf1_s =\n", self.df_indicator_tf1_s, "\n", "indicator_tf1_s =", self.indicator_tf1_s, "\n")
+            self.roc_tf1_s = self.df_indicator_tf1_s['ROC'].iloc[-1]
 
-        #Populating the DF with CCI
+        # Populating the DF with CCI
         # mad function computes the mean deviation based on the price provided
         # typ (typical price) we are using the same value close for all open/close/high/low since we are getting tick by tick data, and forming a candle
         self.df_indicator_tf1_s['typ'] = (self.df_indicator_tf1_s['high'] + self.df_indicator_tf1_s['low'] + self.df_indicator_tf1_s['close']) / 3
         self.df_indicator_tf1_s['sma'] = self.df_indicator_tf1_s['typ'].rolling(self.mov_avg_length_tf1_s).mean()
         self.df_indicator_tf1_s['mad'] = self.df_indicator_tf1_s['typ'].rolling(self.mov_avg_length_tf1_s).apply(lambda x: pd.Series(x).mad())
         self.df_indicator_tf1_s['CCI'] =(self.df_indicator_tf1_s['typ'] - self.df_indicator_tf1_s['sma']) / (0.015 * self.df_indicator_tf1_s['mad'])
-        print("\nSadiq: (CCI Post) df_indicator_tf1_s: \n", self.df_indicator_tf1_s, "\n")
+        #print("\nSadiq: (CCI Post) df_indicator_tf1_s: \n", self.df_indicator_tf1_s, "\n")
         #print("\nSadiq: (CCI Calculation): Typ=", self.df_indicator_tf1_s['typ'], "SMA=", self.df_indicator_tf1_s['sma'], "CCI=", self.df_indicator_tf1_s['CCI'], "\n")
-        #return self.df_indicator_tf1_s['CCI']
+        self.cci_tf1_s = self.df_indicator_tf1_s['CCI'].iloc[-1]
 
     def calc_prev_indicator_tf1_s(self):
         self.n += 1
@@ -193,26 +186,20 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf1_f['low']  = self.df_indicator_tf1_f['close']
         self.df_indicator_tf1_f['indicator_tf1_f'] = TA.HMA(self.df_indicator_tf1_f, self.mov_avg_length_tf1_f) # choose indicator here
         self.indicator_tf1_f = self.df_indicator_tf1_f['indicator_tf1_f'].iloc[-1]
-        print("\nSadiq: Data_TF1_F =", self.data_tf1_f, "\n", "df_indicator_tf1_f =\n", self.df_indicator_tf1_f, "\n", "indicator_tf1_f =", self.indicator_tf1_f, "\n")
+        #print("\nSadiq: Data_TF1_F =", self.data_tf1_f, "\n", "df_indicator_tf1_f =\n", self.df_indicator_tf1_f, "\n", "indicator_tf1_f =", self.indicator_tf1_f, "\n")
 
         #Populating the DF with ROC
         self.roc_counter_tf1_f += 1
-        print("\nSadiq (ROC Pre) Data_TF1_F =", self.data_tf1_f, "ROC_Counter_TF1_F =", self.roc_counter_tf1_f, "Data_TF1_F_LEN =", len(self.data_tf1_f), "mov_avg_length_tf1_f", self.mov_avg_length_tf1_f)
+        #print("\nSadiq (ROC Pre) Data_TF1_F =", self.data_tf1_f, "ROC_Counter_TF1_F =", self.roc_counter_tf1_f, "Data_TF1_F_LEN =", len(self.data_tf1_f), "mov_avg_length_tf1_f", self.mov_avg_length_tf1_f)
         if self.roc_counter_tf1_f < self.mov_avg_length_tf1_f:
             return
-
-        #print("Debug 0", self.df_indicator_tf1_f)
-        #print("Debug 1", self.df_indicator_tf1_f['close'])
-        #print("Debug 2", self.df_indicator_tf1_f['close'].diff(self.mov_avg_length_tf1_f))
-        #print("Debug 3", self.df_indicator_tf1_f['close'].diff(self.mov_avg_length_tf1_f - 1))
-        #print("Debug 4", self.df_indicator_tf1_f['close'].shift(self.mov_avg_length_tf1_f))
-        #print("Debug 4", self.df_indicator_tf1_f['close'].shift(self.mov_avg_length_tf1_f - 1))
 
         if len(self.data_tf1_f) >= self.mov_avg_length_tf1_f:
             self.df_indicator_tf1_f['ROC_M'] = self.df_indicator_tf1_f['close'].diff(self.mov_avg_length_tf1_f - 1)
             self.df_indicator_tf1_f['ROC_N'] = self.df_indicator_tf1_f['close'].shift(self.mov_avg_length_tf1_f - 1)
             self.df_indicator_tf1_f['ROC'] = (self.df_indicator_tf1_f['ROC_M'] / self.df_indicator_tf1_f['ROC_N']) * 100
-            print("\nSadiq: (ROC Post)) Data_TF1_F =\n", self.data_tf1_f, "\n", "df_indicator_tf1_f =\n", self.df_indicator_tf1_f, "\n", "indicator_tf1_f =", self.indicator_tf1_f, "\n")
+            #print("\nSadiq: (ROC Post)) Data_TF1_F =\n", self.data_tf1_f, "\n", "df_indicator_tf1_f =\n", self.df_indicator_tf1_f, "\n", "indicator_tf1_f =", self.indicator_tf1_f, "\n")
+            self.roc_tf1_f = self.df_indicator_tf1_f['ROC'].iloc[-1]
 
         #Populating the DF with CCI
         # mad function computes the mean deviation based on the price provided
@@ -221,8 +208,8 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf1_f['sma'] = self.df_indicator_tf1_f['typ'].rolling(self.mov_avg_length_tf1_f).mean()
         self.df_indicator_tf1_f['mad'] = self.df_indicator_tf1_f['typ'].rolling(self.mov_avg_length_tf1_f).apply(lambda x: pd.Series(x).mad())
         self.df_indicator_tf1_f['CCI'] =(self.df_indicator_tf1_f['typ'] - self.df_indicator_tf1_f['sma']) / (0.015 * self.df_indicator_tf1_f['mad'])
-        print("\nSadiq: (CCI Post) df_indicator_tf1_f: \n", self.df_indicator_tf1_f, "\n")
-        #return self.df_indicator_tf1_f['CCI']
+        #print("\nSadiq: (CCI Post) df_indicator_tf1_f: \n", self.df_indicator_tf1_f, "\n")
+        self.cci_tf1_f = self.df_indicator_tf1_f['CCI'].iloc[-1]
 
     def calc_prev_indicator_tf1_f(self):
         self.p += 1
@@ -237,18 +224,19 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf2_s['low']  = self.df_indicator_tf2_s['close']
         self.df_indicator_tf2_s['indicator_tf2_s'] = TA.WMA(self.df_indicator_tf2_s, self.mov_avg_length_tf2_s) # choose indicator here
         self.indicator_tf2_s = self.df_indicator_tf2_s['indicator_tf2_s'].iloc[-1]
-        print("\nSadiq: Data_TF2_S =", self.data_tf2_s, "\n", "df_indicator_tf2_s =\n", self.df_indicator_tf2_s, "\n", "indicator_tf2_s =", self.indicator_tf2_s, "\n")
+        #print("\nSadiq: Data_TF2_S =", self.data_tf2_s, "\n", "df_indicator_tf2_s =\n", self.df_indicator_tf2_s, "\n", "indicator_tf2_s =", self.indicator_tf2_s, "\n")
 
         #Populating the DF with ROC
         self.roc_counter_tf2_s += 1
-        print("\nSadiq (ROC Pre) Data_TF2_S =", self.data_tf1_s, "ROC_Counter_TF2_S =", self.roc_counter_tf1_s, "Data_TF2_S_LEN =", len(self.data_tf2_s), "mov_avg_length_tf2_s", self.mov_avg_length_tf2_s)
+        #print("\nSadiq (ROC Pre) Data_TF2_S =", self.data_tf1_s, "ROC_Counter_TF2_S =", self.roc_counter_tf1_s, "Data_TF2_S_LEN =", len(self.data_tf2_s), "mov_avg_length_tf2_s", self.mov_avg_length_tf2_s)
         if self.roc_counter_tf2_s < self.mov_avg_length_tf2_s:
             return
         if len(self.data_tf2_s) >= self.mov_avg_length_tf2_s:
             self.df_indicator_tf2_s['ROC_M'] = self.df_indicator_tf2_s['close'].diff(self.mov_avg_length_tf2_s - 1)
             self.df_indicator_tf2_s['ROC_N'] = self.df_indicator_tf2_s['close'].shift(self.mov_avg_length_tf2_s - 1)
             self.df_indicator_tf2_s['ROC'] = (self.df_indicator_tf2_s['ROC_M'] / self.df_indicator_tf2_s['ROC_N']) * 100
-            print("\nSadiq: (ROC Post)) Data_TF2_S =\n", self.data_tf2_s, "\n", "df_indicator_tf2_s =\n", self.df_indicator_tf2_s, "\n", "indicator_tf2_s =", self.indicator_tf2_s, "\n")
+            #print("\nSadiq: (ROC Post)) Data_TF2_S =\n", self.data_tf2_s, "\n", "df_indicator_tf2_s =\n", self.df_indicator_tf2_s, "\n", "indicator_tf2_s =", self.indicator_tf2_s, "\n")
+            self.roc_tf2_s = self.df_indicator_tf2_s['ROC'].iloc[-1]
 
         #Populating the DF with CCI
         # mad function computes the mean deviation based on the price provided
@@ -257,9 +245,9 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf2_s['sma'] = self.df_indicator_tf2_s['typ'].rolling(self.mov_avg_length_tf2_s).mean()
         self.df_indicator_tf2_s['mad'] = self.df_indicator_tf2_s['typ'].rolling(self.mov_avg_length_tf2_s).apply(lambda x: pd.Series(x).mad())
         self.df_indicator_tf2_s['CCI'] =(self.df_indicator_tf2_s['typ'] - self.df_indicator_tf2_s['sma']) / (0.015 * self.df_indicator_tf2_s['mad'])
-        print("\nSadiq: (CCI Post) df_indicator_tf2_s: \n", self.df_indicator_tf2_s, "\n")
+        #print("\nSadiq: (CCI Post) df_indicator_tf2_s: \n", self.df_indicator_tf2_s, "\n")
         #print("\nSadiq: (CCI Calculation): Typ=", self.df_indicator_tf2_s['typ'], "SMA=", self.df_indicator_tf2_s['sma'], "CCI=", self.df_indicator_tf2_s['CCI'], "\n")
-        #return self.df_indicator_tf2_s['CCI']
+        self.cci_tf2_s = self.df_indicator_tf2_s['CCI'].iloc[-1]
 
     def calc_prev_indicator_tf2_s(self):
         self.q += 1
@@ -274,7 +262,7 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf2_f['low']  = self.df_indicator_tf2_f['close']
         self.df_indicator_tf2_f['indicator_tf2_f'] = TA.HMA(self.df_indicator_tf2_f, self.mov_avg_length_tf2_f) # choose indicator here
         self.indicator_tf2_f = self.df_indicator_tf2_f['indicator_tf2_f'].iloc[-1]
-        print("\nSadiq: Data_TF2_F =", self.data_tf2_f, "\n", "df_indicator_tf2_f =\n", self.df_indicator_tf2_f,"\n", "indicator_tf2_f =", self.indicator_tf2_f, "\n")
+        #print("\nSadiq: Data_TF2_F =", self.data_tf2_f, "\n", "df_indicator_tf2_f =\n", self.df_indicator_tf2_f,"\n", "indicator_tf2_f =", self.indicator_tf2_f, "\n")
 
        #Populating the DF with ROC
         self.roc_counter_tf2_f += 1
@@ -285,7 +273,8 @@ class TestApp(EWrapper, EClient):
             self.df_indicator_tf2_f['ROC_M'] = self.df_indicator_tf2_f['close'].diff(self.mov_avg_length_tf2_f - 1)
             self.df_indicator_tf2_f['ROC_N'] = self.df_indicator_tf2_f['close'].shift(self.mov_avg_length_tf2_f - 1)
             self.df_indicator_tf2_f['ROC'] = (self.df_indicator_tf2_f['ROC_M'] / self.df_indicator_tf2_f['ROC_N']) * 100
-            print("\nSadiq: (ROC Post)) Data_TF2_F =\n", self.data_tf2_f, "\n", "df_indicator_tf2_f =\n", self.df_indicator_tf2_f, "\n", "indicator_tf2_f =", self.indicator_tf2_f, "\n")
+            #print("\nSadiq: (ROC Post)) Data_TF2_F =\n", self.data_tf2_f, "\n", "df_indicator_tf2_f =\n", self.df_indicator_tf2_f, "\n", "indicator_tf2_f =", self.indicator_tf2_f, "\n")
+            self.roc_tf2_f = self.df_indicator_tf2_f['ROC'].iloc[-1]
 
         #Populating the DF with CCI
         # mad function computes the mean deviation based on the price provided
@@ -294,8 +283,8 @@ class TestApp(EWrapper, EClient):
         self.df_indicator_tf2_f['sma'] = self.df_indicator_tf2_f['typ'].rolling(self.mov_avg_length_tf2_f).mean()
         self.df_indicator_tf2_f['mad'] = self.df_indicator_tf2_f['typ'].rolling(self.mov_avg_length_tf2_f).apply(lambda x: pd.Series(x).mad())
         self.df_indicator_tf2_f['CCI'] =(self.df_indicator_tf2_f['typ'] - self.df_indicator_tf2_f['sma']) / (0.015 * self.df_indicator_tf2_f['mad'])
-        print("\nSadiq: (CCI Post) df_indicator_tf2_f: \n", self.df_indicator_tf2_f, "\n")
-        #return self.df_indicator_tf2_f['CCI']
+        #print("\nSadiq: (CCI Post) df_indicator_tf2_f: \n", self.df_indicator_tf2_f, "\n")
+        self.cci_tf2_f = self.df_indicator_tf2_f['CCI'].iloc[-1]
 
     def calc_prev_indicator_tf2_f(self):
         self.r += 1
@@ -451,18 +440,22 @@ class TestApp(EWrapper, EClient):
               "TF2_F (HMA):", self.mov_avg_length_tf2_f,"candles \n",
               'Candle_TF1:', str(self.tick_count // self.ticks_per_candle_tf1+1).zfill(3),
               'Tick_TF1:', str(self.tick_count % self.ticks_per_candle_tf1 + 1).zfill(3), "of", str(self.ticks_per_candle_tf1).zfill(3),
-              'ROC_TF1_S:', "{:.2f}".format(self.roc_tf1_s),
+              'ROC_TF1_S:', "{:.4f}".format(self.roc_tf1_s),
+              'CCI_TF1_S:', "{:.2f}".format(self.cci_tf1_s),
               'Ind_TF1_S:', "{:.2f}".format(self.indicator_tf1_s),
               'Prev_Ind_TF1_S:', "{:.2f}".format(self.prev_indicator_tf1_s),
-              'ROC_TF1_F:', "{:.2f}".format(self.roc_tf1_f),
+              'ROC_TF1_F:', "{:.4f}".format(self.roc_tf1_f),
+              'CCI_TF1_F:', "{:.2f}".format(self.cci_tf1_f),
               'Ind_TF1_F:', "{:.2f}".format(self.indicator_tf1_f),
               'Prev_Ind_TF1_F:', "{:.2f}\n".format(self.prev_indicator_tf1_f),
               'Candle_TF2:', str(self.tick_count // self.ticks_per_candle_tf2 + 1).zfill(3),
               'Tick_TF2:', str(self.tick_count % self.ticks_per_candle_tf2 + 1).zfill(3), "of", str(self.ticks_per_candle_tf2).zfill(3),
-              'ROC_TF2_S:', "{:.2f}".format(self.roc_tf2_s),
+              'ROC_TF2_S:', "{:.4f}".format(self.roc_tf2_s),
+              'CCI_TF2_S:', "{:.2f}".format(self.cci_tf2_s),
               'Ind_TF2_S:', "{:.2f}".format(self.indicator_tf2_s),
               'Prev_Ind_TF2_S:', "{:.2f}".format(self.prev_indicator_tf2_s),
-              'ROC_TF2_F:', "{:.2f}".format(self.roc_tf2_f),
+              'ROC_TF2_F:', "{:.4f}".format(self.roc_tf2_f),
+              'CCI_TF2_F:', "{:.2f}".format(self.cci_tf2_f),
               'Ind_TF2_F:', "{:.2f}".format(self.indicator_tf2_f),
               'Prev_Ind_TF2_F:', "{:.2f}".format(self.prev_indicator_tf2_f)
         )
